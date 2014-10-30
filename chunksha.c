16ba7c01ed24520b1189500b2e5f3c6fe2f9e8e4
Iliyan Malchev <malchev@google.com>
2014-10-30 13:26:27 -0700

    functional version
    no makefile, autoconf etc.
    
    gcc chunksha.c -o shachunks -lssl -lcrypto
    
    example:
    	./shachunks -c 512 -z shachunks chunksha.c
    	61e0d18da8c99bd56a60e77716bba75bb3b83a1c        0 shachunks
    	e640da216627c56b6fde57a9618d8a63d62b6045      512 shachunks
    	bf8f256e1af1adf86b4b581800fe3b46d1b384a5     1024 shachunks
    	cee941615ddb752a96c21cff830bd0608dd3310f     1536 shachunks
    	280c9c7f7e17346ebb78c1f4f6e5795bfbf04bb9     2048 shachunks
    	2b1e562fb8fe9bd2d58412c27b5f71468b219bcc     2560 shachunks
    	7e0ff8e85d9a66a41f459a53e9c0ffdff49d3428     3072 shachunks
    	163d3b90badfea058cf02c6e76f9bed054f479da     3584 shachunks
    	b88d01939bfd6049347d0ba35d22bde90908bff9     4096 shachunks
    	2283d9a17dcad80ceb8fb5c0c2fd38526aafcafc     4608 shachunks
    	bbc0f02a6af29f9009353cb3cb54f36f798820c3     5120 shachunks
    	aae96c0fb6a3c19579c53bd04fe2deda47017e9c     5632 shachunks
    	1c669b4f6f8c6ee16566b08014fb28715bb97d31     7680 shachunks
    	7e9fafe9daee2f7ea6e7446b78a48da2c2ef4903     8192 shachunks
    	e5ea9a4dba92bd742ff401fb8881dbb593192402     8704 shachunks
    	b061ea05607e4dca860a202c9a54cb498e0c879c     9216 shachunks
    	565ec7115e0750b2807d0bd81d70d1a6d25bba55     9728 shachunks
    	534f2106dee4c7380a63578fa3fdb1eda1909504    10240 shachunks
    	9f07e4b4ae8a0b929ad7efb849af72a7dab94339    10752 shachunks
    	888b0043306a1242981a01c2ea4c4f6002a7296b    11264 shachunks
    	d14d73cdb5ef65d20af390bfb6b461555e62bc85    11776 shachunks
    	76a879f155832ac44bdf8051fbda42d1827cd15b    12288 shachunks
    	434072e0390ab7773c9cb5e35bcd22fb0de01b15    12800 shachunks
    	cbbef534cad43834423348caf55dd05e9f52546b    13312 shachunks
    	afc6a791fafaf435fcaa2ed29f48dcddeb39aee7        0 chunksha.c
    	2c3719272c60b21aa240e4dd37c71c967cb77795      512 chunksha.c
    	0b995934f4d8864f67305348db79946af0f3ee71     1024 chunksha.c
    	28d4a22d17e32957b8fc1202c79812db3d62d4a8     1536 chunksha.c
    	ccd70ddbbe38f610921a9495b0daa0759ed2d13c     2048 chunksha.c
    	03f86d464bc111af19e7d01041b405d937cbce3f     2560 chunksha.c
    	400757ebb2d2538928e89c99fd1296332bc9f52c     3072 chunksha.c
    
    Signed-off-by: Iliyan Malchev <malchev@google.com>

--- chunksha.c
+++ chunksha.c
@@ -3,6 +3,11 @@
 #include <unistd.h>
 #include <errno.h>
 #include <getopt.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+
 #include <openssl/sha.h>
 
 #define FAILIF(x, msg...) do {        \
@@ -24,26 +29,28 @@ static void help(const char *prog, int err) {
 	_exit(err);
 }
 
-static void process(const char *name);
+static void process(const char *name,
+		    void *chunk, size_t chunk_sz,
+		    const unsigned char *ignore);
+
 
 int main (int argc, char **argv)
 {
-	int chunk = 512;
-	int ignore_zero_chunks = 0;
 	char **files;
+	size_t chunk_sz = 512;
+	void *chunk;
+	unsigned char *ignore = NULL, ignore_sha[SHA_DIGEST_LENGTH];
 
 	int opt;
 	do {
 		opt = getopt(argc, argv, "c:zh");
 		switch (opt) {
 			case 'c':
-				printf("chunk size [%s]\n", optarg);
-				FAILIF(1 != sscanf(optarg, "%u", &chunk), "Invalid chunk-size string\n");
-				FAILIF(chunk & (chunk - 1), "Chunk size is not a power of two!\n");
+				FAILIF(1 != sscanf(optarg, "%lu", &chunk_sz), "Invalid chunk-size string\n");
+				FAILIF(chunk_sz & (chunk_sz - 1), "Chunk size is not a power of two!\n");
 				break;
 			case 'z':
-				printf("ignoring zero-sized chunks\n");
-				ignore_zero_chunks = 1;
+				ignore = ignore_sha;
 				break;
 			case '?':
 				fprintf(stderr, "unknown command-line option %c\n", optopt);
@@ -59,16 +66,72 @@ int main (int argc, char **argv)
 	FAILIF(optind >= argc, "Expecting file name(s)!\n");
 
 	files = argv + optind;
+	chunk = malloc(chunk_sz);
+	FAILIF(!chunk, "Could not allocate chunk of size %lu\n", chunk_sz);
+
+	if (ignore) {
+		memset(chunk, 0, chunk_sz);
+		SHA1(chunk, chunk_sz, ignore);
+	}
+
 	while (*files) {
-		process(*files);
+		process(*files, chunk, chunk_sz, ignore);
 		files++;
 	}
 
 	return 0;
 }
 
-
-static void process(const char *name)
+static void process(const char *name,
+		    void *chunk, size_t chunk_sz,
+		    const unsigned char *ignore)
 {
-	printf("file [%s]\n", name);
+	int fd;
+	int rc;
+	unsigned char sha[SHA_DIGEST_LENGTH];
+	struct stat st;
+	size_t num_chunks;
+	size_t i;
+
+	fd = open (name, O_RDONLY);
+	FAILIF(fd < 0, "Could not open [%s]: %s\n", name, strerror(errno));
+
+	rc = fstat(fd, &st);
+	FAILIF(rc < 0, "Could not fstat [%s]: %s\n", name, strerror(errno));
+
+	i = 0;
+	num_chunks = st.st_size / chunk_sz;
+	while (1) {
+		rc = read(fd, chunk, chunk_sz);
+		if (!rc)
+			break;
+		if (rc < 0) {
+			FAILIF(errno != EINTR,
+			       "Could not read from [%s]: %s\n",
+			       name, strerror(errno));
+			continue;
+		}
+
+		FAILIF(rc < chunk_sz && i < num_chunks - 1,
+		       "Unexpected chunk size %u for chunk %lu (out of %lu)!\n",
+		       rc, i, num_chunks);
+
+		SHA1(chunk, rc, sha);
+
+		if (ignore && !memcmp(ignore, sha, sizeof(sha))) {
+			i++;
+			continue;
+		}
+
+		printf("%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x %8ld %s\n",
+		       sha[ 0], sha[ 1], sha[ 2], sha[ 3], sha[ 4],
+		       sha[ 5], sha[ 6], sha[ 7], sha[ 8], sha[ 9],
+		       sha[10], sha[11], sha[12], sha[13], sha[14],
+		       sha[15], sha[16], sha[17], sha[18], sha[19],
+		       i * chunk_sz, name);
+
+		i++;
+	}
+
+	close (fd);
 }
